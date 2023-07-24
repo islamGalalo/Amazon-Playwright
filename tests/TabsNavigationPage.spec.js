52644e141641f3e04af120b04cc45552420a35d1
islamGalalo <islamgalal.qatesting@gmail.com>
2023-07-24 09:36:19 +0300

    added recently

---
+++ tests/TabsNavigationPage.spec.js
@@ -0,0 +1,15 @@
+import {test , expect} from '@playwright/test';
+import { TabsNavigationPage } from '../Pages/TabsNavigationPage';
+test ('test', async ({page})=> {
+const tabs = new TabsNavigationPage ();
+tabs.tap1();
+tabs.tap2();
+tabs.tap3();
+tabs.tap4();
+tabs.tap5();
+tabs.tap6();
+tabs.tap7();
+tabs.tap8();
+tabs.tap9();
+tabs.tap10();
+})
